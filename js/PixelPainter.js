8aa759fb66dfa7a01f1c2d613aaa29972bbe8adb
yukio yamamoto <aj.yamamoto@me.com>
2015-02-26 20:10:35 -1000

    added files from origin

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -8,7 +8,7 @@ $(function(){
     var swatch_size = "20px";
     var square_size = "36px";
 
-    for(var i = 0; i < num_swatches; i++){
+    for(var i = 0; i < num_swatches; i++) {
       var new_swatch = $('<div>', {"class" : "color"});
       new_swatch.css({
         'background-color' : color_str(),
@@ -31,6 +31,9 @@ $(function(){
     $('#controls').append(swatches, erase, clear);
     // we have created the basic layout of the content div by appending all the neccessary tags
     var grid = $('<div>', {"class" : "grid"});
+    var rowWidth = (Number(square_size.substring(0, square_size.length-2)) + 2) * width;
+    console.log(square_size);
+
 
     for (var j = 0; j < height; j++) {
       for(var k = 0; k < width; k++) {
@@ -38,11 +41,14 @@ $(function(){
         new_square.css({
           'border' : "solid #000000 1px",
           'height' : square_size,
-          'width' : square_size
+          'width' : square_size,
+          'display' : 'inline-block'
         });
         grid.append(new_square);
       } 
     }
+    $('.grid').css({'width' : rowWidth.toString() + 'px'});
+    console.log(rowWidth);
 
     $('#artboard').append(grid);
   }
@@ -54,6 +60,7 @@ $(function(){
 
     return "rgb("+randomColor1+", "+randomColor2+", "+randomColor3+")";
   }
+
 
   PixelPainter(20, 20);
 
